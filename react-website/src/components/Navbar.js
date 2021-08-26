9a54fca0f61be8f17615f32a434a902b24f81bf2
Estelle <estelle.wu117@gmail.com>
2021-08-26 16:51:25 +1200

    navbar

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -6,7 +6,8 @@ import './Navbar.css';
 function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
-
+    //console.log(click) //false
+    //console.log(setClick) //dispatchAction()
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
@@ -30,7 +31,7 @@ function Navbar() {
                 <div className='navbar-container'>
                     <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                         TRVL
-                        <i class='fab fa-typo3' />
+                        <i className='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
