f4c93f9e64a179c329cd6f80be3b40cf07291f30
Estelle <estelle.wu117@gmail.com>
2021-08-31 15:57:37 +1200

    update

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import './App.css'
-import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+
+import Navbar from './components/Navbar'
 import Home from './components/pages/Home'
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
@@ -19,7 +20,6 @@ function App() {
           <Route exact path="/sign-up" component={SignUp}/>
         </Switch>
       </Router>
-
     </>
   );
 }
