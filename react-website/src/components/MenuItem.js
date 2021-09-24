1fe7733d39b6d95436b9a872f2e8372c156b22e5
Estelle <estelle.wu117@gmail.com>
2021-09-24 15:10:42 +1200

    add dropdown on home page

---
+++ react-website/src/components/MenuItem.js
@@ -0,0 +1,18 @@
+export const MenuItem=[
+    {title: 'Market',
+    path: '/mark',
+    cName: 'dropdown-link'
+    },
+    {title: 'Develop',
+    path: '/develop',
+    cName: 'dropdown-link'
+    },
+    {title: 'Design',
+    path: '/design',
+    cName: 'dropdown-link'
+    },
+    {title: 'Consult',
+    path: '/consult',
+    cName: 'dropdown-link'
+    }
+]
--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -2,12 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Dropdown from './Dropdown';
 
 function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
+    const [dropdown, setDropdown]=useState(false)
 
     const closeMobileMenu = () => setClick(false);
+
+    //dropdown
+    const onMouseEnter=()=>{
+        if(window.innerWidth<960){
+            setDropdown(false)
+        }else{
+            setDropdown(true)
+        }
+    }
+
+    const onMouseLeave=()=>{
+        if(window.innerWidth<960){
+            setDropdown(false)
+        }else{
+            setDropdown(true)
+        }
+    }
 
     const showButton = () => {
         if (window.innerWidth <= 960) {
@@ -17,6 +36,7 @@ function Navbar() {
         }
     };
 
+    
     useEffect(() => {
         showButton();
     }, []);
@@ -43,14 +63,14 @@ function Navbar() {
                                 Home
                             </Link>
                         </li>
-                        <li className='nav-item'>
+                        <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                             <Link
                                 to='/services'
                                 className='nav-links'
                                 onClick={closeMobileMenu}
                             >
-                                Services
-                            </Link>
+                                Services <i className='fas fa-caret-down'/>
+                            </Link>{dropdown && <Dropdown />}
                         </li>
                         <li className='nav-item'>
                             <Link
