9d7bc710573984dc164105f6c34bd0ca3ae82e02
Estelle <estelle.wu117@gmail.com>
2021-09-28 16:55:47 +1300

    dropdown menu

--- react-website/src/components/Dropdown.js
+++ react-website/src/components/Dropdown.js
@@ -2,6 +2,10 @@ import React,{useState} from "react";
 import {MenuItem} from './MenuItem'
 import { Link } from "react-router-dom";
 import './Dropdown.css'
+import img1 from '../assets/img-1.jpg'
+import img2 from '../assets/img-2.jpg'
+import img3 from '../assets/img-3.jpg'
+import img4 from '../assets/img-5.png'
 
 function Dropdown(){
 
@@ -10,17 +14,31 @@ function Dropdown(){
 
     return (
         <>
-            <ul onClick={handleClick} className={click? 'dropdown-menu clicked':'dropdown-menu'}>
-                {MenuItem.map((item,index)=>{
-                    return (
-                        <li key={index}>
-                            <Link className={item.cName} to={item.path} onClick={()=>setClick(false)}>
-                                {item.title}
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ul>
+        <div className='container'>
+                <div onClick={handleClick} className={click? 'dropdown-menu clicked':'dropdown-menu'}>
+                    <div className='left'>
+                        {MenuItem.map((item,index)=>{
+                        return (
+                                <li key={index}>
+                                    <Link className={item.cName} to={item.path} onClick={()=>setClick(false)}>
+                                        {item.title} 
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </div>
+                    <li className='line'></li>
+                    
+                    <div className='right'>
+                       <li><img src={img1} alt="" className='drop-img'/></li> 
+                       <li><img src={img2} alt="" className='drop-img'/></li> 
+                       <li><img src={img3} alt="" className='drop-img'/></li> 
+                       <li><img src={img4} alt="" className='drop-img'/></li> 
+                    </div>
+                </div>
+        </div>
+        
+            
             
         </>
     )
--- react-website/src/components/MenuItem.js
+++ react-website/src/components/MenuItem.js
@@ -1,18 +1,29 @@
+
+
 export const MenuItem=[
-    {title: 'Market',
-    path: '/mark',
-    cName: 'dropdown-link'
+    {
+        title: 'Market',
+        path: '/mark',
+        cName: 'dropdown-link'
     },
-    {title: 'Develop',
-    path: '/develop',
-    cName: 'dropdown-link'
+    {
+        title: 'Develop',
+        path: '/develop',
+        cName: 'dropdown-link',
     },
-    {title: 'Design',
-    path: '/design',
-    cName: 'dropdown-link'
+    {
+        title: 'Design',
+        path: '/design',
+        cName: 'dropdown-link',
     },
-    {title: 'Consult',
-    path: '/consult',
-    cName: 'dropdown-link'
+    {
+        title: 'Consult',
+        path: '/consult',
+        cName: 'dropdown-link',
+    },
+    {
+        title: 'Consult',
+        path: '/consult',
+        cName: 'dropdown-link',
     }
 ]
